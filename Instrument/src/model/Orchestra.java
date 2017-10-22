af6c4a48de729f36bf1717d3fc3e26851c970168
Sazi Valair <sazivalair@gmail.com>
2017-10-22 15:54:11 -0700

    Updated Instrument solution

--- Instrument/src/model/Orchestra.java
+++ Instrument/src/model/Orchestra.java
@@ -14,4 +14,37 @@ public class Orchestra {
         this.brassInstruments = brassInstruments;
         this.stringInstruments = stringInstruments;
     }
+
+    public void addBrassInstrument(BrassInstrument b) {
+        if(!brassInstruments.contains(b)) {
+            brassInstruments.add(b);
+            b.setOrchestra(this);
+        }
+    }
+
+    public void addStringInstrument(StringInstrument s) {
+        if(!stringInstruments.contains(s)) {
+            stringInstruments.add(s);
+            s.setOrchestra(this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Orchestra)) return false;
+
+        Orchestra orchestra = (Orchestra) o;
+
+        if (brassInstruments != null ? !brassInstruments.equals(orchestra.brassInstruments) : orchestra.brassInstruments != null)
+            return false;
+        return stringInstruments != null ? stringInstruments.equals(orchestra.stringInstruments) : orchestra.stringInstruments == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = brassInstruments != null ? brassInstruments.hashCode() : 0;
+        result = 31 * result + (stringInstruments != null ? stringInstruments.hashCode() : 0);
+        return result;
+    }
 }
--- Instrument/src/model/instrument/StringInstrument.java
+++ Instrument/src/model/instrument/StringInstrument.java
@@ -9,4 +9,26 @@ public abstract class StringInstrument implements Instrument {
     public StringInstrument(Orchestra orchestra) {
         this.orchestra = orchestra;
     }
+
+    public void setOrchestra(Orchestra orchestra) {
+        if (!this.orchestra.equals(orchestra)) {
+            this.orchestra = orchestra;
+            orchestra.addStringInstrument(this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StringInstrument)) return false;
+
+        StringInstrument that = (StringInstrument) o;
+
+        return orchestra != null ? orchestra.equals(that.orchestra) : that.orchestra == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return orchestra != null ? orchestra.hashCode() : 0;
+    }
 }
